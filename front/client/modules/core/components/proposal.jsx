6f690fff30e5c3ae37a37d0eed52e90f84ef03ed
Adam Dawkins <adamdawkins@gmail.com>
2016-05-11 16:23:02 +0100

    Header styling

--- front/client/modules/core/components/proposal.jsx
+++ front/client/modules/core/components/proposal.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import Cover from './cover';
-const Proposal = ({proposal}) => (
-  <div>
-    <Cover {...proposal} />
-  <div dangerouslySetInnerHTML={{__html: proposal.body}} />
-</div>
-);
+import ScrollReveal from 'scrollreveal';
+
+class Proposal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerShown: false,
+    };
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+  handleScroll(event) {
+    const { scrollTop } = event.srcElement.body;
+    this.setState({ scrollTop });
+    const article = document.querySelector('.article');
+    if ((article.offsetTop <= scrollTop) && !this.state.headerShown) {
+      console.log('time to reveal');
+      window.sr = ScrollReveal();
+      sr.reveal(document.getElementById('header'), {
+        duration: 500,
+      });
+      this.setState({headerShown: true});
+    }
+  }
+  render() {
+    const { proposal } = this.props;
+    return (
+      <div>
+        <header id="header" className="header">
+          <h1 className="title">{proposal.title}</h1>
+        </header>
+        <Cover {...proposal} />
+        <div dangerouslySetInnerHTML={{__html: proposal.body}} />
+      </div>
+  );
+  }
+}
 
 export default Proposal;
